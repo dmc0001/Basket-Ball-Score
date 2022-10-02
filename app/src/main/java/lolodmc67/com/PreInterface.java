6886c072be26629d72584742dcb6f3ec11580b7c
lorenzo medici <lorenzomedici2001@gmail.com>
2022-10-02 23:56:26 +0200

    0.0.4

--- app/src/main/java/lolodmc67/com/PreInterface.java
+++ app/src/main/java/lolodmc67/com/PreInterface.java
@@ -1,15 +1,23 @@
 package lolodmc67.com;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class PreInterface extends AppCompatActivity {
-
+ConstraintLayout preInterface;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pre_interface);
+        preInterface = findViewById(R.id.preInterface);
+        preInterface.setOnClickListener(View -> {
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        });
 
     }
 
