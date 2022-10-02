0f2674bf409b724b61fba9e298c738bda8fa71b0
lorenzo medici <lorenzomedici2001@gmail.com>
2022-10-02 23:19:03 +0200

    0.0.3.1

---
+++ app/src/main/java/lolodmc67/com/PreInterface.java
@@ -0,0 +1,16 @@
+package lolodmc67.com;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PreInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pre_interface);
+
+    }
+
+}
