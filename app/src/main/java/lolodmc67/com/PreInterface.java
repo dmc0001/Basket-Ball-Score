d6fb6cd442929cb825601c017e7cd348ed4b5462
lorenzo medici <lorenzomedici2001@gmail.com>
2022-10-06 13:21:32 +0200

    0.0.6

--- app/src/main/java/lolodmc67/com/PreInterface.java
+++ app/src/main/java/lolodmc67/com/PreInterface.java
@@ -8,17 +8,32 @@ import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class PreInterface extends AppCompatActivity {
     LinearLayout preInterface;
+    Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pre_interface);
-        preInterface = findViewById(R.id.preInterface);
-        preInterface.setOnClickListener(View -> {
-            Intent intent = new Intent(this,MainActivity.class);
-            startActivity(intent);
-        });
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(PreInterface.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
+
+//        preInterface = findViewById(R.id.preInterface);
+//        preInterface.setOnClickListener(View -> {
+//            Intent intent = new Intent(this,MainActivity.class);
+//            startActivity(intent);
+//        });
 
     }
 
