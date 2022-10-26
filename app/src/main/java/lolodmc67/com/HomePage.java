7b637db5b852d27436073c9e280567e6d942770b
lorenzo medici <lorenzomedici2001@gmail.com>
2022-10-26 18:12:04 +0200

    0.1.2

--- app/src/main/java/lolodmc67/com/HomePage.java
+++ app/src/main/java/lolodmc67/com/HomePage.java
@@ -4,19 +4,27 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 public class HomePage extends AppCompatActivity {
-
+FloatingActionButton floatingButton;
+String nameTeamA,nameTeamB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,9 +43,25 @@ public class HomePage extends AppCompatActivity {
         myrecycler.setLayoutManager(new LinearLayoutManager(this));
         myrecycler.setAdapter(adapter);
 
-        CardView cardView = findViewById(R.id.card);
+        floatingButton= findViewById(R.id.floating_button);
+        floatingButton.setOnClickListener(view -> {
 
+            Dialog dialog = new Dialog(HomePage.this);
+            dialog.setContentView(R.layout.dialog);
+            EditText teamA = dialog.findViewById(R.id.team_a_);
+            EditText teamB = dialog.findViewById(R.id.team_b_);
+            Button save =dialog.findViewById(R.id.save);
 
+            nameTeamA =teamA.getText().toString();
+            nameTeamB =teamB.getText().toString();
+            save.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+        });
 
     }
 }
+
